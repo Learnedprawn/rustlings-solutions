4316c0ab1f5d4bf49ea4690bd6f563eeebca1c07
Learnedprawn <vineshparalkar@gmail.com>
2025-07-03 20:01:02 +0530

    47

--- 11_hashmaps/hashmaps1.rs
+++ 11_hashmaps/hashmaps1.rs
@@ -8,10 +8,14 @@ use std::collections::HashMap;
 
 fn fruit_basket() -> HashMap<String, u32> {
     // TODO: Declare the hash map.
-    // let mut basket =
+    let mut basket = HashMap::new();
 
     // Two bananas are already given for you :)
     basket.insert(String::from("banana"), 2);
+    basket.insert(String::from("apple"), 5);
+    basket.insert(String::from("watermelon"), 8);
+    basket.insert(String::from("banana"), 9);
+    basket.insert(String::from("mango"), 1);
 
     // TODO: Put more fruits in your basket.
 
--- quizzes/quiz2.rs
+++ quizzes/quiz2.rs
@@ -24,10 +24,24 @@ enum Command {
 }
 
 mod my_module {
+    use std::collections::btree_map::Range;
+
     use super::Command;
 
     // TODO: Complete the function as described above.
-    // pub fn transformer(input: ???) -> ??? { ??? }
+    pub fn transformer(input: Vec<(String, Command)>) -> Vec<String> {
+        let mut output = Vec::new();
+        for element in input {
+            let (mut the_string, the_command) = element;
+            let new_string = match the_command {
+                Command::Trim => String::from(the_string.trim()),
+                Command::Uppercase => the_string.to_uppercase(),
+                Command::Append(times) => the_string + &"bar".repeat(times),
+            };
+            output.push(new_string);
+        }
+        output
+    }
 }
 
 fn main() {
@@ -37,7 +51,7 @@ fn main() {
 #[cfg(test)]
 mod tests {
     // TODO: What do we need to import to have `transformer` in scope?
-    // use ???;
+    use super::my_module::transformer;
     use super::Command;
 
     #[test]
